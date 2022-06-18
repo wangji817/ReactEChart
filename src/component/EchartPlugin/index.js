e58cd966b1676da0dc8853327337ca1c417a9cdb
wangji817 <526153861@qq.com>
2022-06-18 15:03:47 +0800

    更新代码

--- src/component/EchartPlugin/index.js
+++ src/component/EchartPlugin/index.js
@@ -2,13 +2,12 @@ import './index.scss';
 
 import React, { useEffect, useState, } from 'react';
 import * as echarts from 'echarts';
-import { LineOption, LegendOption, CandlestickOption, CandlestickShOption } from '../../echartData/';
+import { LineOption, LegendOption, CandlestickOption, CandlestickShOption } from '../../echartData/';//这部分还可以优化
 
 const EchartPlugin = (props) => {
     const [myCharts, setMyChart] = useState([]);
     const options = [LineOption, LegendOption, CandlestickOption, CandlestickShOption];
     useEffect(() => {
-        const new_myCharts = [];
         const size = {
             width: (document.querySelector(".EchartPlugin").clientWidth / 2) - 8.5,
             height: ((document.documentElement.clientHeight - 60) / 2),
@@ -16,9 +15,8 @@ const EchartPlugin = (props) => {
         options.forEach(item => {
             let myChart = echarts.init(document.getElementById(item.id), 'dark', size);
             myChart.setOption(item);
-            new_myCharts.push(myChart);
+            setMyChart([...myCharts, item]);
         });
-        setMyChart(new_myCharts);
     }, []);
 
     useEffect(() => {
